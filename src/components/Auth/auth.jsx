321d04e924878ae9f22adde3906cf39611e8f3e5
Osmon <osmonabdimannan@gmail.com>
2021-02-25 22:07:50 +0600

    auth dialog

--- src/components/Auth/auth.jsx
+++ src/components/Auth/auth.jsx
@@ -1,6 +1,191 @@
-import { Dialog } from "@material-ui/core";
-import React from "react";
+import {
+  Checkbox,
+  Dialog,
+  DialogContent,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  withStyles,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { GoldButton } from "shared/Buttons/buttons";
 
-export function Auth({ open, handleClose }) {
-  return <Dialog open={open}></Dialog>;
+export function Auth({ open, handleClose, login, setLogin }) {
+  const theme = useTheme();
+  const sm = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <Dialog open={open} onClose={handleClose} aria-labelledby='auth-dialog'>
+      <DialogContent
+        style={{
+          backgroundColor: "#2A2B31",
+          padding: "10px 100px",
+        }}
+      >
+        {login ? <SingIn sm={sm} /> : <SingUp sm={sm} />}
+
+        <div className='flex_box' style={{ margin: "20px 0" }}>
+          <Typography
+            variant='body2'
+            style={{ width: "100%", textAlign: "center" }}
+          >
+            {login ? "У вас нет учетной записи?" : "У вас уже есть аккаунт?"}{" "}
+            <span
+              style={{ color: "#FF9900", cursor: "pointer" }}
+              onClick={() => setLogin(!login)}
+            >
+              {login ? "Зарегистрироваться" : "Войти"}
+            </span>
+          </Typography>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 }
+
+function SingIn({ sm }) {
+  const [fogetPassword, setPassword] = useState(false);
+
+  function submitHandler(e) {
+    e.preventDefault();
+  }
+  return (
+    <>
+      {sm ? (
+        <p
+          className='title'
+          style={{ fontSize: 25, textAlign: "center", marginBottom: 40 }}
+        >
+          {fogetPassword ? "Восстановление пароля" : "Вход"}
+        </p>
+      ) : null}
+      <form onSubmit={submitHandler}>
+        <Typography variant='body2'>Ваша почта</Typography>
+        <ThemeInput
+          margin='dense'
+          placeholder='Введите почту'
+          name='email'
+          type='email'
+          variant='outlined'
+          fullWidth
+        />
+        {!fogetPassword ? (
+          <>
+            <Typography variant='body2'>Пароль</Typography>
+            <ThemeInput
+              margin='dense'
+              placeholder='Введите пароль'
+              name='password'
+              type='password'
+              variant='outlined'
+              fullWidth
+            />
+            <div
+              className='flex_box'
+              style={{ justifyContent: "space-between" }}
+            >
+              <span className='flex_box'>
+                <Checkbox name='rememberMe' color='primary' />
+                <Typography variant='body2'>Запомнить пароль</Typography>
+              </span>
+
+              <span
+                className='nav_link'
+                style={{ fontSize: 16 }}
+                onClick={() => setPassword(true)}
+              >
+                Забыли пароль?
+              </span>
+            </div>
+          </>
+        ) : null}
+
+        <GoldButton style={{ ...btnStyle }} type={"submit"}>
+          {fogetPassword ? "Отправить на почту" : "Войти"}
+        </GoldButton>
+      </form>
+    </>
+  );
+}
+
+function SingUp({ sm }) {
+  function submitHandler(e) {
+    e.preventDefault();
+  }
+  return (
+    <>
+      {sm ? (
+        <p
+          className='title'
+          style={{ fontSize: 25, textAlign: "center", marginBottom: 40 }}
+        >
+          Регистрация
+        </p>
+      ) : null}
+      <form onSubmit={submitHandler}>
+        <Typography variant='body2'>Имя</Typography>
+        <ThemeInput
+          margin='dense'
+          placeholder='Введите Ваше имя'
+          name='username'
+          type='text'
+          variant='outlined'
+          fullWidth
+        />
+        <Typography variant='body2'>Почта</Typography>
+        <ThemeInput
+          margin='dense'
+          placeholder='Введите почту'
+          name='email'
+          type='email'
+          variant='outlined'
+          fullWidth
+        />
+        <Typography variant='body2'>Пароль</Typography>
+        <ThemeInput
+          margin='dense'
+          placeholder='Введите пароль'
+          name='password'
+          type='password'
+          variant='outlined'
+          fullWidth
+        />
+        <ThemeInput
+          margin='dense'
+          placeholder='Повторите пароль'
+          name='password2'
+          type='password'
+          variant='outlined'
+          fullWidth
+        />
+        <GoldButton style={{ ...btnStyle }} type={"submit"}>
+          Зарегистрироваться
+        </GoldButton>
+      </form>
+    </>
+  );
+}
+
+let btnStyle = {
+  minHeight: 40,
+  maxHeight: 40,
+  marginTop: 40,
+  minWidth: 195,
+  marginLeft: "50%",
+  fontSize: 16,
+  transform: "translateX(-50%)",
+};
+
+const ThemeInput = withStyles({
+  root: {
+    "& .MuiOutlinedInput-root": {
+      fontWeight: 300,
+      fontSize: 16,
+      fontFamily: "Roboto",
+      color: "#595959",
+      border: "none",
+      backgroundColor: "#F5F5F5",
+    },
+  },
+})(TextField);
